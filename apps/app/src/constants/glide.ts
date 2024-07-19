4903fb4b470f6f958e61104b9a05b9c31affde4f
Complexlity <nwalozielijah@gmail.com>
2024-07-19 10:19:19 +0100

    chore: add pay with glide

---
+++ apps/app/src/constants/glide.ts
@@ -0,0 +1,12 @@
+import { Chain, chains, Config, createGlideConfig } from '@paywithglide/glide-js'
+
+// console.log("https://paywithglide.xyz/")
+const projectId = process.env.NEXT_PUBLIC_GLIDE_PROJECT_ID
+if (!projectId) throw new Error('Glide project Id missing from .env')
+
+const GLIDE_CONFIG: Config<readonly Chain[]> = createGlideConfig({
+  projectId: projectId,
+  chains: [chains.base, chains.optimism]
+})
+
+export { GLIDE_CONFIG }
