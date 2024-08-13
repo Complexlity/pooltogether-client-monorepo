45b6c48623e4e95846fc6d4df4a54d8135b64616
Complexlity <nwalozielijah@gmail.com>
2024-08-13 18:18:05 +0100

    feat: add new glide payment amount

--- apps/app/src/hooks/glide/useCreateSessionAtIntervals.ts
+++ apps/app/src/hooks/glide/useCreateSessionAtIntervals.ts
@@ -1,10 +1,21 @@
-import { Vault, vaultABI } from '@generationsoftware/hyperstructure-client-js'
-import { CAIP19, createSession } from '@paywithglide/glide-js'
-import { useQuery } from '@tanstack/react-query'
+import {
+  NETWORK,
+  USDC_TOKEN_ADDRESSES,
+  Vault,
+  vaultABI
+} from '@generationsoftware/hyperstructure-client-js'
+import {
+  QUERY_KEYS,
+  useTokenPrices,
+  useVaultTokenPrice
+} from '@generationsoftware/hyperstructure-react-hooks'
+import { CAIP19, createSession, estimatePaymentAmount } from '@paywithglide/glide-js'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { Address, parseUnits } from 'viem'
 import { useAccount } from 'wagmi'
 import { crossTokenDetails } from '@components/Modals/DepositModal/DepositForm'
 import { GLIDE_CONFIG as glideConfig } from '@constants/glide'
+import { useEthPriceInUsd } from '@hooks/useEthPrice'
 
 const GLIDE_SESSION_REFETCH_INTERVAL = 2 * 60 * 1000
 /**
@@ -20,16 +31,21 @@ export const useCreateSessionAtIntervals = (
   vault: Vault,
   crossTokenDetails: crossTokenDetails
 ) => {
+  const { data } = useTokenPrices(crossTokenDetails.chainId, [crossTokenDetails.address])
   const { address: userAddress } = useAccount()
-
+  const { data: vaultToken } = useVaultTokenPrice(vault)
+  const { data: ethToUsdPrice } = useEthPriceInUsd()
   const tokenPriceUsd = Number(crossTokenDetails.balanceUSD) / Number(crossTokenDetails.balance)
+  const tokenPriceEth = tokenPriceUsd / Math.round(ethToUsdPrice!)
+  const tokenPriceInVaultToken = tokenPriceEth / vaultToken?.price!
+
   let vaultDecimals = vault.decimals
   if (!vaultDecimals) {
     vaultDecimals = 6
   }
 
-  let depositAmount = parseUnits(`${tokenPriceUsd * Number(amount)}`, vaultDecimals)
-
+  // Send 1 usd in the deposited token
+  let depositAmount = parseUnits(`${1 / tokenPriceUsd}`, vaultDecimals)
   let {
     data: session,
     isFetching,
@@ -42,6 +58,8 @@ export const useCreateSessionAtIntervals = (
     queryKey: [`glideSession`],
     queryFn: async () => {
       const parameters = {
+        //Actual payment amount that is used by glide
+        paymentAmount: Number(amount),
         chainId: vault.chainId,
         account: userAddress as Address,
         abi: vaultABI,
